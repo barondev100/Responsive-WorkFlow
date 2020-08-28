b33895e0b7a13b4549cc4cacf127eb541204f486
barondev100 <barondev100@gmail.com>
2020-08-29 00:06:06 +0100

    all done

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  // Hamburger menu Toogle
+  $('.nav-toggle').click(function(){
+    $('.main-nav').toggleClass('is-Open');
+    $('.hamburger').toggleClass('is-Open');
+  })
+})
